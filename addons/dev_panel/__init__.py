e3b153f7fca41bd96d32a289557742fddd42e26e
ikezaki <39111330+izumiikezaki@users.noreply.github.com>
2024-06-15 22:20:35 +0900

    devpanel追加

---
+++ addons/dev_panel/__init__.py
@@ -0,0 +1,50 @@
+from bpy import utils
+import bpy
+
+bl_info = {
+    "name": "ikz_dev_panel",
+    "author": "izumi_ikezaki",
+    "version": (1, 0),
+    "blender": (3, 6, 0),
+    "location": "",
+    "description": "",
+    "warning": "test",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Object",
+}
+
+
+class HelloWorldPanel(bpy.types.Panel):
+    bl_category = "ikz"
+    bl_idname = __name__
+    bl_label = "for dev"
+    bl_description = "開発に使うやつ"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+
+    # bl_context = ""
+    def draw(self, context):
+        layout = self.layout
+
+        obj = context.object
+
+        # row = layout.row()
+        # row.label(text="Active object is: " + obj.name)
+        # row = layout.row()
+        # row.prop(obj, "name")
+
+        row = layout.row()
+        row.operator("script.reload")
+
+
+def register():
+    bpy.utils.register_class(HelloWorldPanel)
+
+
+def unregister():
+    bpy.utils.unregister_class(HelloWorldPanel)
+
+
+if __name__ == "__main__":
+    register()
