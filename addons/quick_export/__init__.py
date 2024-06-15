b135c11c9fd9bead2f43a895d8b3d0e031a96364
ikezaki <39111330+izumiikezaki@users.noreply.github.com>
2024-06-15 21:29:20 +0900

    fix

--- addons/quick_export/__init__.py
+++ addons/quick_export/__init__.py
@@ -30,22 +30,22 @@ class MyPropGrp(bpy.types.PropertyGroup):
     )
 
 
-class QuickExport(bpy.types.Operator):
+class IKZ_OT_QuickExport(bpy.types.Operator):
     bl_idname = "ikz.quick_export"
     bl_label = "quick export"
     bl_description = "指定の場所に自分用の設定でエクスポートするやつ"
 
-    default_path = bpy.context.blend_data.filepath.rstrip(
-        bpy.path.basename(bpy.context.blend_data.filepath)
-    )
-    default_name = bpy.path.basename(bpy.context.blend_data.filepath).split(".")[0]
     extension = ".fbx"
 
     def execute(self, context):
+        default_path = bpy.context.blend_data.filepath.rstrip(
+            bpy.path.basename(bpy.context.blend_data.filepath)
+        )
+        default_name = bpy.path.basename(bpy.context.blend_data.filepath).split(".")[0]
 
         setting = getattr(context.scene, "ikz_qe_props", None)
-        p = setting.option_path or self.default_path
-        n = setting.option_name or self.default_name
+        p = setting.option_path or default_path
+        n = setting.option_name or default_name
 
         filepath = p + n + self.extension
 
@@ -67,9 +67,11 @@ class QuickExport(bpy.types.Operator):
         return {"FINISHED"}
 
 
-class QuickExportLayoutPanel(bpy.types.Panel):
+class IKZ_PT_QuickExport(bpy.types.Panel):
     bl_category = "ikz"
+    bl_idname = __name__
     bl_label = "quick export"
+    bl_description = "エクスポートする"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     # bl_context = ""
@@ -89,10 +91,10 @@ class QuickExportLayoutPanel(bpy.types.Panel):
 
         row = layout.row()
         row.scale_y = 2.0
-        row.operator(QuickExport.bl_idname)
+        row.operator(IKZ_OT_QuickExport.bl_idname)
 
 
-register_classes = (QuickExportLayoutPanel, QuickExport, MyPropGrp)
+register_classes = (IKZ_PT_QuickExport, IKZ_OT_QuickExport, MyPropGrp)
 
 
 def register():
